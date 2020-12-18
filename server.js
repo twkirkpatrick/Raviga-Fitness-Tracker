a00f273b895887d1f377d5bca17d80c200e65b1d
Tanner Kirkpatrick <twk4491@gmail.com>
2020-12-18 10:58:21 -0500

    styling before submission

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", 
 {   
-    useNewUrlParser: true, 
+    useNewUrlParser:true, 
     useUnifiedTopology:true, 
     useCreateIndex:true,
     useFindAndModify:false 
